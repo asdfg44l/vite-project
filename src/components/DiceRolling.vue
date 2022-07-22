efd662fe3c8c46950fffd88ce983364a15b8b121
Pineapple Bun <35952622+asdfg44l@users.noreply.github.com>
2022-07-22 16:34:10 +0800

    modify: way to fix transition shadow

--- src/components/DiceRolling.vue
+++ src/components/DiceRolling.vue
@@ -1,25 +1,27 @@
 <template>
     <div class="w-600 mx-auto">
         <p>hello dice</p>
-        <button @click="rollDice" :disabled="!isAllowRolling">躑骰子</button>
+        <button @click="rollDice">躑骰子</button>
         <button @click="diceReset">Reset</button>
+        <p v-if="isShowOutcome">Outcome</p>
         <div class="box" :ref="(el) => dice.containerRef = el">
-            <transition-group
+            <transition
                 :name="dice.transName"
                 :css="isAllowTransition"
-                @after-leave="diceAfterLeave"
+                @animationend="onDiceAnimationEnd"
             >
-                <img v-if="dice.currentType == 'purple'" key="purple" class="dice"  src="@/assets/dice_defaultPurple.png" width="120" height="120" />
-                <img v-else-if="dice.currentType  == 'blue'" key="blue" class="dice"  src="@/assets/dice_defaultBlue.png" width="120" height="120" />
-                <img v-else-if="dice.currentType  == 'win'" key="win" class="dice"  src="@/assets/dice_win.png" width="120" height="120" />
-                <img v-else-if="dice.currentType  == 'lose'" key="lose" class="dice"  src="@/assets/dice_lose.png" width="120" height="120" />
-            </transition-group>
+                <img v-if="dice.currentType == 'purple'" key="purple" class="dice"  src="@/assets/dice_defaultPurple.png" width="120" height="120" alt/>
+                <img v-else-if="dice.currentType  == 'blue'" key="blue" class="dice"  src="@/assets/dice_defaultBlue.png" width="120" height="120" alt/>
+                <img v-else-if="dice.currentType  == 'win'" key="win" class="dice"  src="@/assets/dice_win.png" width="120" height="120" alt/>
+                <img v-else-if="dice.currentType  == 'lose'" key="lose" class="dice"  src="@/assets/dice_lose.png" width="120" height="120" alt/>
+            </transition>
         </div>
     </div>
 </template>    
 
 <script>
-import { reactive, ref, onMounted, onUnmounted, watch, nextTick } from 'vue'
+import { computed } from '@vue/reactivity'
+import { reactive, ref, onMounted, onUnmounted } from 'vue'
 export default {
     name: "DiceRolling",
     setup() {        
@@ -32,9 +34,11 @@ export default {
             transName: 'dice-trans', //dice transition name
             containerRef: null // dice box element
         })
-        const isFirstRolling = ref(true)
-        const isAllowRolling = ref(true)
         const isAllowTransition = ref(true)
+        const isShowOutcome = ref(false)
+
+        //computed
+        const isDefaultDice = computed(() => dice.gleamList.indexOf(dice.currentType) !== -1)
 
         //animation
         //骰子自動漸變
@@ -59,33 +63,29 @@ export default {
 
         //change transition name to roll dice
         async function rollDice() {
-            cancelAnimationFrame(dice.gleamAnimationFrame)
-            dice.transName = 'dice-rolling'
+            isShowOutcome.value = false //清除上一輪的結果
+            cancelAnimationFrame(dice.gleamAnimationFrame) //清掉骰子漸變效果
+            dice.transName = 'dice-rolling' //transition 換成 rolling效果
 
             // Notice: try to clear dice shadow
-            if(dice.gleamList.indexOf(dice.currentType) !== -1) {
-                dice.containerRef.children[0].setAttribute('height', 0)
+            if(isDefaultDice.value) {
+                dice.containerRef.children[0].className = 'd-none'
             }
 
-            await nextTick()
-
             dice.currentType = dice.currentType === 'win' ? 'lose' : 'win'
-            
-            if(isFirstRolling.value) isAllowRolling.value = false
-            isFirstRolling.value = false
         }
 
         //transition hooks
-        function diceAfterLeave(el) {
-            isAllowRolling.value = true
+        function onDiceAnimationEnd(e) {
+            const isDiceRollingTrans = e.animationName.match('dice-rolling')
+
+            if(isDiceRollingTrans) isShowOutcome.value = true
         }
 
         //reset
         //reset dice status
         async function diceReset() {
-            //避免重置時露出破綻
-            isAllowRolling.value = false
-            isFirstRolling.value = true //重置為初次擲骰
+            isShowOutcome.value = false
             //避免重置骰子樣式時產生 transition
             isAllowTransition.value = false
             dice.currentType = 'purple'
@@ -94,15 +94,6 @@ export default {
             cancelAnimationFrame(dice.gleamAnimationFrame)
             autoSwitchDice()
         }
-
-        // watch(
-        //     () => dice.currentType,
-        //     val => {
-        //         if(val === 'win' | val === 'lose') {
-        //             console.log('Dice Rolling!!!!!!')
-        //         } 
-        //     }
-        // )
 
         onMounted(() => {
             autoSwitchDice()
@@ -114,9 +105,9 @@ export default {
             dice,
             rollDice,
             diceReset,
-            isAllowRolling,
             isAllowTransition,
-            diceAfterLeave
+            isShowOutcome,
+            onDiceAnimationEnd
         }
     }
 }
@@ -159,10 +150,10 @@ export default {
         animation: dice-fade-in 3s reverse;
     }
     &-rolling-enter-active {
-        animation: dice-fade-in 1s ease, dice-rolling .2s 4 linear, dice-bounce .8s linear !important;
+        animation: dice-fade-in 0.8s ease, dice-rolling .2s 4 linear, dice-bounce .8s linear !important;
     }
     &-rolling-leave-active {
-        animation: dice-fade-in 3s reverse, dice-rolling .2s 4 linear, dice-bounce .8s linear !important;
+        animation: dice-fade-in 0.8s reverse, dice-rolling .2s 4 linear, dice-bounce .8s linear !important;
     }
 }
 
